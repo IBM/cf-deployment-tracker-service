d23fc703a136ab8a7ed4fa1a4ec46fa0d381c7db
Tommy Li <Tommy.chaoping.li@ibm.com>
2018-01-04 17:46:15 -0800

    add redis cache and fix auth bug (#32)
    * add redis cache and fix auth bug
    
    * style clean up
    
    * update dependencies to the lastest stable version

--- app.js
+++ app.js
@@ -23,10 +23,11 @@ var express = require("express"),
     metric = require('./metric'),
     fs = require("fs");
 
-
-dotenv.load();
-
 var appEnv = cfenv.getAppEnv();
+
+if (appEnv.isLocal) {
+    dotenv.load();
+}
 
 var forceSslIfNotLocal = function(req, res, next) {
     if (appEnv.isLocal) {
@@ -87,10 +88,10 @@ function authenticate() {
         if (appEnv.isLocal) {
             return next();
         }
-        console.log(request.session);
+
         if (!request.isAuthenticated() || request.session.passport.user === undefined) {
             response.redirect("/auth/sso");
-            return next();
+            return;
         }
 
         var email = request.session.passport.user.id,
@@ -103,8 +104,9 @@ function authenticate() {
             response.render("error", {
                 message: "You must be an IBM'er to use this app"
             });
+        } else {
+            return next();
         }
-        return next();
     };
 }
 
@@ -142,7 +144,7 @@ var SSO_CLIENT_ID = (process.env.SSO_CLIENT_ID || " "),
     SSO_CLIENT_SECRET = (process.env.SSO_CLIENT_SECRET || " "),
     SSO_URL = (process.env.SSO_URL || "");
 
-var callbackURL = "https://deployment-tracker.mybluemix.net" + "/auth/sso/callback";
+var callbackURL = "https://metrics-tracker.mybluemix.net" + "/auth/sso/callback";
 if (process.env.BASE_URL) {
     callbackURL = "https://" + process.env.BASE_URL + "/auth/sso/callback";
 }
@@ -180,9 +182,9 @@ app.get("/auth/sso/callback", [forceSslIfNotLocal,
         res.redirect("/stats");
     });
 
-app.get("/logout", forceSslIfNotLocal, function(request, response) {
-    passport._strategy("openidconnect").logout(request, response, appEnv.url);
-});
+// app.get("/logout", forceSslIfNotLocal, function(request, response) {
+//     passport._strategy("openidconnect").logout(request, response, appEnv.url);
+// });
 
 (function(app) {
     if (process.env.VCAP_SERVICES) {
@@ -830,6 +832,26 @@ app.get("/stats/:hash", [forceSslIfNotLocal, authenticate()], function(req, res)
 
 // Get badge of metrics for a specific repo
 app.get("/stats/:hash/badge.svg", forceSslIfNotLocal, function(req, res) {
+    var hash = req.params.hash;
+    res.set({
+        "Content-Type": "image/svg+xml",
+        "Cache-Control": "no-cache",
+        "Expires": 0
+    });
+    if (appEnv.isLocal) {
+        generateBadge(req, res);
+    } else {
+        sessionStore.client.get("badge-" + hash, function(err, result) {
+            if (err || !result) {
+                generateBadge(req, res);
+            } else {
+                res.render("badge.xml", JSON.parse(result));
+            }
+        });
+    }
+});
+
+function generateBadge(req, res) {
     var app = req.app,
         deploymentTrackerDb = app.get("deployment-tracker-db");
 
@@ -839,14 +861,12 @@ app.get("/stats/:hash/badge.svg", forceSslIfNotLocal, function(req, res) {
     var eventsDb = deploymentTrackerDb.use("events"),
         hash = req.params.hash;
 
-    //TODO: Consider caching this data with Redis
     eventsDb.view("deployments", "by_repo_hash_unique", {
         startkey: [hash],
         endkey: [hash, {}, {}, {}, {}],
         group_level: 5
     }, function(err, body) {
         var count = body.rows.length;
-        //TODO: Rename this variable
         var svgData = {
             left: "IBM Cloud Deployments",
             right: count.toString(),
@@ -856,17 +876,36 @@ app.get("/stats/:hash/badge.svg", forceSslIfNotLocal, function(req, res) {
         svgData.totalWidth = svgData.leftWidth + svgData.rightWidth;
         svgData.leftX = svgData.leftWidth / 2 + 1;
         svgData.rightX = svgData.leftWidth + svgData.rightWidth / 2 - 1;
-        res.set({
-            "Content-Type": "image/svg+xml",
-            "Cache-Control": "no-cache",
-            "Expires": 0
-        });
+        // If the app is on CF, cache the button data with Redis
+        if (!appEnv.isLocal) {
+            sessionStore.client.setex("badge-" + hash, 300, JSON.stringify(svgData));
+        }
         res.render("badge.xml", svgData);
     });
-});
+}
 
 // Get a "Deploy to IBM Cloud" button for a specific repo
 app.get("/stats/:hash/button.svg", forceSslIfNotLocal, function(req, res) {
+    var hash = req.params.hash;
+    res.set({
+        "Content-Type": "image/svg+xml",
+        "Cache-Control": "no-cache",
+        "Expires": 0
+    });
+    if (appEnv.isLocal) {
+        generateButton(req, res);
+    } else {
+        sessionStore.client.get("button-" + hash, function(err, result) {
+            if (err || !result) {
+                generateButton(req, res);
+            } else {
+                res.render("button.xml", JSON.parse(result));
+            }
+        });
+    }
+});
+
+function generateButton(req, res) {
     var app = req.app,
         deploymentTrackerDb = app.get("deployment-tracker-db");
 
@@ -875,15 +914,12 @@ app.get("/stats/:hash/button.svg", forceSslIfNotLocal, function(req, res) {
     }
     var eventsDb = deploymentTrackerDb.use("events"),
         hash = req.params.hash;
-
-    //TODO: Consider caching this data with Redis
     eventsDb.view("deployments", "by_repo_hash_unique", {
         startkey: [hash],
         endkey: [hash, {}, {}, {}, {}],
         group_level: 5
     }, function(err, body) {
         var count = body.rows.length;
-        //TODO: Rename this variable
         var svgData = {
             left: "Deploy to IBM Cloud",
             right: count.toString(),
@@ -897,14 +933,13 @@ app.get("/stats/:hash/button.svg", forceSslIfNotLocal, function(req, res) {
         svgData.totalWidth = svgData.totalWidth + 48;
         svgData.leftX = svgData.leftX + 48;
         svgData.rightX = svgData.rightX + 48;
-        res.set({
-            "Content-Type": "image/svg+xml",
-            "Cache-Control": "no-cache",
-            "Expires": 0
-        });
+        // If the app is on CF, cache the button data with Redis
+        if (!appEnv.isLocal) {
+            sessionStore.client.setex("button-" + hash, 300, JSON.stringify(svgData));
+        }
         res.render("button.xml", svgData);
     });
-});
+}
 
 function track(req, res) {
     var app = req.app;
